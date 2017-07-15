b34cf9897143dc3b20e1e98640fa22f7b353ad73
Bill Peregoy <bill.peregoy@endurance.com>
2017-07-15 17:52:56 -0400

    Dispatcher can now dispatch!

--- lib/compute_farm.ex
+++ lib/compute_farm.ex
@@ -1,25 +1,46 @@
 defmodule ComputeFarm do
-  def server(dispatcher_pid, name) do
-    send(dispatcher_pid, {:register, %{name: name}})
+  def server(dispatcher_pid, name, options \\ %{register: true}) do
+    if options.register do
+      send(dispatcher_pid, {:register, %{name: name, pid: self}})
+    end
+
     receive do 
       message -> IO.puts("Server #{name} received #{message}")
+      ComputeFarm.server(dispatcher_pid, name, %{register: false})
     end
-    ComputeFarm.server(dispatcher_pid, name)
   end
 
-  def dispatcher(servers \\ []) do
-    receive do
+  def dispatcher(servers \\ %{}) do
+    servers = receive do
       :servers ->
-        server_string = Enum.join(servers, ", ")
-        IO.puts("Available server list: #{server_string}")
+        server_string = Map.values(servers)
+                        |> Enum.map(fn elem -> elem.name end)
+                        |> Enum.join(", ")
 
-      {:register, server_info} ->
-        servers = [server_info.name | servers]
+        IO.puts("Available server list: #{server_string}")
+        servers
+
+      {:register, %{name: name}=server_info} ->
         IO.puts("Registered new server: #{server_info.name}")
+        Map.put(servers, name, server_info)
+
+      {:dispatch, server_name} ->
+        case Map.get(servers, server_name) do
+          nil ->
+            IO.puts("Cannot find server #{server_name}")
+          %{name: name, pid: pid} ->
+            IO.puts("Senidng to #{name}")
+            send(pid, "message")
+          _ ->
+            IO.puts("Unexpected condition. Aborting")
+        end
+        servers
 
       _ ->
         IO.puts("Unknown command")
+        servers
     end
+
     ComputeFarm.dispatcher(servers)
   end
 end
